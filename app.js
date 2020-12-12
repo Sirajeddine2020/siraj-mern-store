f00693f97d8c5fa5e64f1fe2f5f27c82376549ce
Sirajeddine <71317422+Sirajeddine2020@users.noreply.github.com>
2020-12-12 21:27:26 +0100

    Update app.js

--- app.js
+++ app.js
@@ -6,10 +6,7 @@ const authenticate = require('./api/middleware/authenticate');
 
 
 const db= "mongodb+srv://fedi:Fedi123456@cluster0.eviib.mongodb.net/SirajStore?retryWrites=true&w=majority"
-//const db="mongodb+srv://sirajdinho@hotmail.com :MongoDB2020**@cluster0.eviib.mongodb.net/SirajStore?retryWrites=true&w=majority"
-//const db= "mongodb://localhost:27017/student";
-//const db= "mongodb+srv://'+ process.env.MONGODB_USERNAME +':'+ process.env.MONGODB_PASSWORD@cluster0.eviib.mongodb.net/SirajStore?retryWrites=true&w=majority"
-//const db= 'mongodb+srv://'+ process.env.MONGODB_USERNAME +':'+ process.env.MONGODB_PASSWORD +'@cluster0-axd8v.mongodb.net/mystore?retryWrites=true&w=majority';
+
 mongoose
        .connect(db,		
            {    useNewUrlParser: true,
