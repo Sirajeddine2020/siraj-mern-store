7a15c36f539d94a1dcef9ab74eda32ebeae2a006
Sirajeddine <71317422+Sirajeddine2020@users.noreply.github.com>
2020-12-12 21:28:59 +0100

    Update urlConfig.js

--- admin/src/urlConfig.js
+++ admin/src/urlConfig.js
@@ -1,5 +1,6 @@
 //export const api = 'http://localhost:2019/';
 export const api = 'https://siraj-mern-store.herokuapp.com/';
 export const generatePublicUrl = (fileName) => {
-    return `http://localhost:2019/public/${fileName}`;
+    return `https://siraj-mern-store.herokuapp.com/public/${fileName}`;
+   // return `http://localhost:2019/public/${fileName}`;
 }
